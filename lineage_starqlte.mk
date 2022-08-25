4dd28a01df6137e499a5a8950342e6b205ebfc2e
Mohammad Afaneh <mohammad.afaneh92@gmail.com>
2022-08-25 11:11:16 +0300

    Initial skeleton device tree

---
+++ lineage_starqlte.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (C) 2021-2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from starqlte device
+$(call inherit-product, device/samsung/starqlte/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_starqlte
+PRODUCT_DEVICE := starqlte
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-G960U
